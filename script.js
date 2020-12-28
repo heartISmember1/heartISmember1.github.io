26aa618110957d5fa948674dc03af2a4f13f1920
heartISmember1 <anhld@allexceed.co.jp>
2020-12-28 16:54:35 +0700

    tuyentnd 20201228

--- script.js
+++ script.js
@@ -1,56 +1,15 @@
-$(function() {
+function changeMe()
+  {
+    document.getElementById("changeImg").src = "images/" + getRandomImage();	
+	document.getElementById("changeText").innerHTML = getRandomText();
+  }
+function getRandomImage() {
+  var images = ["安田.jpg","永井.jpg","奥間_正式.jpg","岡田.png","近藤.jpg","今岡.jpg","佐藤.jpg","坂田.jpg","榊原.jpg","山本.jpg","山本_本気モード.jpg","舟橋.jpg","勝間.jpg","小畑.jpg","松井.jpg","神原.jpg","水口.jpg","清水.jpg","早川.jpg","糟谷.jpg","村上.jpg","村瀬.jpg","太田.jpg","中川.jpg","長谷川.jpg","坪内.jpg","都築.jpg","藤本.jpg","白谷.jpg","飯田.jpg","飯田圭.jpg","武藤.jpg","伏見.jpg","野村.jpg","野々山.jpg","鈴木誉人.jpg"];
 
-// var stage = new Konva.Stage({
-//   container: 'canvas',   // id of container <div>
-//   width: 500,
-//   height: 500
-// });
-//
-// var layer = new Konva.Layer();
-//
-// var circle = new Konva.Circle({
-//   x: stage.getWidth() / 2,
-//   y: stage.getHeight() / 2,
-//   radius: 200,
-//   fill: 'red',
-//   stroke: 'black',
-//   strokeWidth: 4
-// });
-//
-// layer.add(circle);
-// stage.add(layer);
-
-/////////////////////////////////////////////////
-
-$('#start').on('click', function() {
-
-  var input = $('#input').val().replace(/^\n/gm, '').replace(/\n$/, '').split('\n');
-
-  console.log('input: ' + input);
-  $('#output').css('font-weight', 'normal');
-  $('#output').css('color', 'black');
-
-  (function myLoop (i) {
-     setTimeout(function () {
-
-        var random = input[Math.floor(Math.random() * input.length)];
-        $('#output').text(random);
-        console.log(random);
-
-        if (--i) myLoop(i);     //  decrement i and call myLoop again if i > 0
-        if (i === 0) {
-          $('#output').css('font-weight', 'bold');
-          $('#output').css('color', 'red');
-        }
-     }, 50);
-  })(50);
-
-
-
-
-
-});
-
-
-
-}); // $
+  return images[Math.floor(Math.random() * images.length)];
+}
+function getRandomText() {
+	var textArea = document.getElementById('area1').value;     // plain JavaScript
+	var res = textArea.split("\n");
+	return res[Math.floor(Math.random() * res.length)];
+ }
